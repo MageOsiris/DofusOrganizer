2679c7251865d17e1a03245c9699147f5e57492e
gaeta <gaeta@MAGE_TOWER>
2023-03-05 22:47:38 +0100

    Ajoutez des fichiers projet.

---
+++ DofusOrganizer/Base.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace DofusOrganizer
+{
+    partial class Base
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox1 = new NumericUpDown();
+            textBox2 = new NumericUpDown();
+            textBox3 = new NumericUpDown();
+            textBox4 = new NumericUpDown();
+            textBox5 = new NumericUpDown();
+            textBox6 = new NumericUpDown();
+            textBox7 = new NumericUpDown();
+            textBox8 = new NumericUpDown();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)textBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)textBox8).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(358, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Detection of dofus instances";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 20);
+            label1.TabIndex = 1;
+            label1.Text = "List of instances =>";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 100);
+            label2.Name = "label2";
+            label2.AccessibleName = "Account1";
+            label2.Size = new Size(71, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Account1";
+            label2.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 140);
+            label3.Name = "label3";
+            label3.Size = new Size(71, 20);
+            label3.TabIndex = 3;
+            label3.AccessibleName = "Account2";
+            label3.Text = "Account2";
+            label3.Visible = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 180);
+            label4.Name = "label4";
+            label4.Size = new Size(71, 20);
+            label4.TabIndex = 4;
+            label4.AccessibleName = "Account3";
+            label4.Text = "Account3";
+            label4.Visible = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 220);
+            label5.Name = "label5";
+            label5.Size = new Size(71, 20);
+            label5.TabIndex = 5;
+            label5.AccessibleName = "Account4";
+            label5.Text = "Account4";
+            label5.Visible = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(12, 260);
+            label6.Name = "label6";
+            label6.Size = new Size(71, 20);
+            label6.TabIndex = 6;
+            label6.AccessibleName = "Account5";
+            label6.Text = "Account5";
+            label6.Visible = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 300);
+            label7.Name = "label7";
+            label7.Size = new Size(71, 20);
+            label7.TabIndex = 7;
+            label7.AccessibleName = "Account6";
+            label7.Text = "Account6";
+            label7.Visible = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(12, 340);
+            label8.Name = "label8";
+            label8.Size = new Size(71, 20);
+            label8.TabIndex = 8;
+            label8.AccessibleName = "Account7";
+            label8.Text = "Account7";
+            label8.Visible = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(12, 380);
+            label9.Name = "label9";
+            label9.Size = new Size(71, 20);
+            label9.TabIndex = 9;
+            label9.AccessibleName = "Account8";
+            label9.Text = "Account8";
+            label9.Visible = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(245, 100);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 40);
+            textBox1.TabIndex = 10;
+            textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(245, 140);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 40);
+            textBox2.TabIndex = 11;
+            textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(245, 180);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 40);
+            textBox3.TabIndex = 12;
+            textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(245, 220);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 40);
+            textBox4.TabIndex = 13;
+            textBox4.Visible = false;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(245, 260);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 40);
+            textBox5.TabIndex = 14;
+            textBox5.Visible = false;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(245, 300);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(125, 40);
+            textBox6.TabIndex = 15;
+            textBox6.Visible = false;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(245, 340);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(125, 40);
+            textBox7.TabIndex = 16;
+            textBox7.Visible = false;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(245, 380);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(125, 40);
+            textBox8.TabIndex = 17;
+            textBox8.Visible = false;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(312, 450);
+            label10.Name = "label10";
+            label10.Size = new Size(0, 20);
+            label10.TabIndex = 20;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(12, 475);
+            label11.Name = "label11";
+            label11.Size = new Size(78, 20);
+            label11.TabIndex = 21;
+            label11.Text = "Left Arrow";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(292, 475);
+            label12.Name = "label12";
+            label12.Size = new Size(88, 20);
+            label12.TabIndex = 22;
+            label12.Text = "Right Arrow";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(12, 510);
+            button2.Name = "button2";
+            button2.Size = new Size(358, 29);
+            button2.TabIndex = 23;
+            button2.Text = "Apply";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click_1;
+            // 
+            // Base
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(382, 551);
+            Controls.Add(button2);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Base";
+            Text = "Base";
+            ((System.ComponentModel.ISupportInitialize)textBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)textBox8).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private NumericUpDown textBox1;
+        private NumericUpDown textBox2;
+        private NumericUpDown textBox3;
+        private NumericUpDown textBox4;
+        private NumericUpDown textBox5;
+        private NumericUpDown textBox6;
+        private NumericUpDown textBox7;
+        private NumericUpDown textBox8;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Button button2;
+    }
+}
---
+++ DofusOrganizer/Base.cs
@@ -0,0 +1,159 @@
+﻿using System.Data;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DofusOrganizer
+{
+    public partial class Base : Form
+    {
+        KeyboardHook hookLeft = new KeyboardHook();
+        KeyboardHook hookRight = new KeyboardHook();
+        KeyboardHook hookCurrent = new KeyboardHook();
+
+        List<DofusElement> dofusElements = new List<DofusElement>();
+
+        int currentIndex = 0;
+
+        public Base()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dofusElements = new List<DofusElement> ();
+
+            Process[] processCollection = Process.GetProcesses().Where(x => x.ProcessName == "Dofus").ToArray();
+
+            Label[] labels = Controls.OfType<Label>().Where(x => x.AccessibleName is not null).Where(x => x.AccessibleName.Contains("Account")).ToArray();
+            NumericUpDown[] textBoxes = Controls.OfType<NumericUpDown>().Where(x => x.Name.Contains("textBox")).ToArray();
+
+            NumericUpDown textBox;
+            Label label;
+
+            var cpt = 0;
+
+            foreach (Process p in processCollection)
+            {
+                if (cpt != 7)
+                {
+                    label = labels.FirstOrDefault(x => x.AccessibleName == "Account" + (cpt + 1));
+                    textBox = textBoxes.FirstOrDefault(x => x.Name == "textBox" + (cpt + 1));
+
+                    label.Text = p.MainWindowTitle.Split(" - ")[0];
+
+                    dofusElements.Add(new DofusElement() { Process = p, Name = p.MainWindowTitle, Rank = 0, Cpt = (cpt+1).ToString() });
+
+                    label.Visible = true;
+                    textBox.Visible = true;
+                    cpt++;
+                }
+
+                Console.WriteLine(p.ProcessName);
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            hookLeft.Dispose();
+            hookRight.Dispose();
+            hookCurrent.Dispose();
+            hookLeft = new KeyboardHook();
+            hookRight = new KeyboardHook();
+            hookCurrent = new KeyboardHook();
+
+            NumericUpDown[] textBoxes = Controls.OfType<NumericUpDown>().Where(x => x.Name.Contains("textBox")).ToArray();
+
+            foreach (NumericUpDown p in textBoxes)
+            {
+                DofusElement dofusElement = dofusElements.FirstOrDefault(x => x.Cpt == p.Name.Split("textBox")[1]);
+                if (dofusElement is not null)
+                {
+                    dofusElement.Rank = (int)p.Value;
+                }
+            }
+
+            dofusElements = dofusElements.OrderBy(x => x.Rank).ThenBy(x => x.Name).ToList();
+
+            // register the event that is fired after the key press.
+            hookRight.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(Descend);
+            // register the control + alt + F12 combination as hot key.
+            hookRight.RegisterHotKey(DofusOrganizer.ModifierKeys.None, Keys.Right);
+
+            // register the event that is fired after the key press.
+            hookLeft.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(Ascend);
+            // register the control + alt + F12 combination as hot key.
+            hookLeft.RegisterHotKey(DofusOrganizer.ModifierKeys.None, Keys.Left);
+
+            // register the event that is fired after the key press.
+            hookCurrent.KeyPressed +=
+                new EventHandler<KeyPressedEventArgs>(Current);
+            // register the control + alt + F12 combination as hot key.
+            hookCurrent.RegisterHotKey(DofusOrganizer.ModifierKeys.Control, Keys.Up);
+
+        }
+
+        [DllImport("user32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_MAXIMIZE = 3;
+
+        private void Ascend(object sender, KeyPressedEventArgs e)
+        {
+            GoNextWindow(true);
+        }
+
+        private void Descend(object sender, KeyPressedEventArgs e)
+        {
+            GoNextWindow(false);
+        }
+
+        private void Current(object sender, KeyPressedEventArgs e)
+        {
+            SetForegroundWindow(dofusElements[currentIndex].Process.MainWindowHandle);
+            ShowWindow(dofusElements[currentIndex].Process.MainWindowHandle, SW_MAXIMIZE);
+        }
+
+        public void GoNextWindow(bool isAscend)
+        {
+            if (currentIndex > -1)
+            {
+                if (isAscend && currentIndex == 0)
+                    currentIndex = dofusElements.Count - 1;
+                else if (isAscend)
+                    currentIndex--;
+                else if (!isAscend && currentIndex == (dofusElements.Count - 1))
+                    currentIndex = 0;
+                else
+                    currentIndex++;
+            }
+            else
+            {
+                currentIndex = 0;
+            }
+
+            if (currentIndex > -1)
+            {
+                SetForegroundWindow(dofusElements[currentIndex].Process.MainWindowHandle);
+                ShowWindow(dofusElements[currentIndex].Process.MainWindowHandle, SW_MAXIMIZE);
+            }
+        }
+    }
+
+    public class DofusElement
+    {
+        public Process Process;
+        public string Name;
+        public int Rank;
+        public string Cpt;
+    }
+}
